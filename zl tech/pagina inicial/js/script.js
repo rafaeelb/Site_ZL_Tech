fb2b65eaf12f22551fb4567816f759f7269c0bb1
Rafael <rafael2004benicio@gmail.com>
2024-09-22 18:29:11 -0300

    init commit

---
+++ zl tech/pagina inicial/js/script.js
@@ -0,0 +1,42 @@
+// efeito que some os elementos
+
+// criação de um evento
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    const fadeInElements = document.querySelectorAll('.fade-in');
+    fadeInElements.forEach(element => observer.observe(element));
+
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+    }
+
+});
+
